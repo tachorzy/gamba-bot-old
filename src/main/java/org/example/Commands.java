c6bc331d919d7c2eca78a27e2b5215d8e5135c0b
John Nguyen <jnguyen11011@gmail.com>
2022-05-16 13:57:29 -0500

    Revised commands

--- src/main/java/org/example/Commands.java
+++ src/main/java/org/example/Commands.java
@@ -4,34 +4,43 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-
 import java.awt.*;
 
+/*
+    When user sends a message it is an event
+    We extract a command and split into words which is a string array args
+    arg[0] is the command rest is parameters
+
+    note there is a bug where it finds * and another character when you sys out
+    not sure if it leads into issues atm so far its fine
+*/
 public class Commands extends ListenerAdapter {
-    public String prefix = "&";
+    public char PREFIX = '*';
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         String[] args = event.getMessage().getContentRaw().split(" ");
-        if(args[0].equalsIgnoreCase(prefix + "test")){
-            event.getChannel().sendMessage("HEHE").queue();
-            event.getChannel().sendMessage(event.getAuthor().getAvatarUrl()).queue();
-        }
-        else if(args[0].equalsIgnoreCase(prefix + "test2")){
-            event.getChannel().sendMessage("<@970172253448843284>").queue();
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setColor(Color.red);
-            eb.setTitle("At the Crib");
-            eb.setDescription("Partying with DJ POPCAT !FISHPOG");
-            eb.addField("DRINKING TEARS FROM SUSSYBOI","HOLD L THERE IS NO HOPE THERE IS NO CURVE",false);
-            eb.addField("QUOTE","RIP BOZO",false);
 
+        if(args[0].charAt(0) == PREFIX){
+            switch(args[0]){
+                case "*fishpog":
+                    try{
+                        event.getChannel().sendMessage("https://c.tenor.com/p4MNhgEwIGwAAAAC/poggers-fish-lol.gif").queue();
+                    }
+                    catch(Exception er){
+                        System.out.println(er);
+                    }
+                    break;
+                case "*annoy":
+                    try{
+                        event.getChannel().sendMessage("<@107022278838996992>").queue();
+                    }
+                    catch(Exception er){
+                        System.out.println(er);
+                    }
+                    break;
+            }
+        }
 
-            event.getChannel().sendMessageEmbeds(eb.build()).queue();
-            eb.clear();
-        }
-        else if(args[0].equalsIgnoreCase(prefix + "fishpog")){
-            event.getChannel().sendMessage("https://c.tenor.com/p4MNhgEwIGwAAAAC/poggers-fish-lol.gif").queue();
-        }
     }
 }
--- src/main/java/org/example/DiscordBot.java
+++ src/main/java/org/example/DiscordBot.java
@@ -4,10 +4,14 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import javax.security.auth.login.LoginException;
 
+/*
+    Create Default takes token and creates the bot
+    Event listener listen to user commands
+*/
 public  class DiscordBot {
     public static void main(String[] args) throws LoginException {
         JDA bot = JDABuilder.createDefault("OTc1MjcwNzk3NDk1MTM2MzE2.GB8dgr.iwA6VMVsq8AJH9T9frwDLO_kiST2RUeD3w2Yus")
-                .setActivity(Activity.playing("pop pop"))
+                .setActivity(Activity.playing("Slots #Gamba Addiction"))
                 .build();
         System.out.println("Bot is up and running!");
         bot.addEventListener(new Commands());
